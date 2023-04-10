ee93e52f96ff734b4b66f65668f88885e0c9d036
victor <joaovicif2021@gmail.com>
2023-04-09 21:33:17 -0300

    Mudanças no 'teste-xml.php'

--- src/teste-xml.php
+++ src/teste-xml.php
@@ -4,14 +4,33 @@ require_once 'Pessoa.php';
 require_once 'ExportadorDePessoaEmXml.php';
 
 //Arrange
-$pessoa = new Pessoa('Víctor', new DateTimeImmutable('2005-11-14'));
-$exportador = new ExportadorDePessoaEmXml($pessoa);
+$exportador = new ExportadorDePessoaEmXml(new class extends Pessoa {
+    public function __construct()
+    {
+    }
+
+    public function idade(): int
+    {
+        return 17;
+    }
+
+    public function nome(): string
+    {
+        return 'Víctor';
+    }
+});
 
 //Act
 $xml = $exportador->exportaEmXml();
 
 //Assert
-$conteudoEsperado = '<pessoa><nome>Víctor</nome><idade>17</idade></pessoa>'; #TESTE FALHOU
+$conteudoEsperado = "
+            <pessoa>
+                <nome>Víctor</nome>
+                <idade>17</idade>
+            </pessoa>
+        ";
+
 
 if ($conteudoEsperado === $xml) {
     echo 'TESTE OK' . PHP_EOL;
